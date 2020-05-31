5391f15f1a85d0cd7d76e3d45a954d2d26b42812
song <mr.songjintao@gmail.com>
2020-05-31 21:20:27 +0800

    updata

--- src/api/index.js
+++ src/api/index.js
@@ -1,8 +1,11 @@
 import axios from './axios.js'
 
-//登陆
+/**
+ * 登陆
+ */
 export const login = (username, password) => axios.post('/login', { username: username, password: password }, {});
 
-//搜索
-
+/**
+ * 搜索电影或明星
+ */
 export const searchMovie = (searchVal) => axios.post('/searchMovie', { searchVal: searchVal }, {});
